452c341f61671d5bdf2863069cb6532c4a3f3116
circle-oo <woni8708@naver.com>
2021-01-20 03:31:24 +0900

    fix error

--- env/cartpole.py
+++ env/cartpole.py
@@ -6,9 +6,8 @@ from env.baseenv import _Env
 class CartPole(_Env):
     def __init__(self, render):
         super().__init__('CartPole-v1', render)
-        self.action_number = 1
+        self.action_number = 2
         self.state_number = 4
     
     def action(self, act):
-        return 1 if act > 0 else 0
-        
+        return act
