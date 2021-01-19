e6f232d1f43e6e55b58535a615c2f90a345fd887
circle-oo <woni8708@naver.com>
2021-01-20 08:22:11 +0900

    fix state shape pong and breakout

--- env/breakout.py
+++ env/breakout.py
@@ -7,23 +7,21 @@ class Breakout(_Env):
     def __init__(self, render):
         super().__init__('Breakout-v0', render)
         self.action_number = 4
-        self.state_number = (1, 84, 84)
+        self.state_number = (1, 210, 160)
     
     def reset(self):
         state = super().reset()
         state = np.array(state)
         state = state.mean(axis=2)
-        state = np.resize(state, (84, 84))
-        state = state.reshape((1, 84, 84))
+        state = state.reshape((1, 210, 160))
         return state
     
     def action(self, act):
         return act
-        
+    
     def step(self, act):
         state, reward, done, _ = super().step(act)
         state = np.array(state)
         state = state.mean(axis=2)
-        state = np.resize(state, (84, 84))
-        state = state.reshape((1, 84, 84))
+        state = state.reshape((1, 210, 160))
         return state, reward, done, _
--- env/pong.py
+++ env/pong.py
@@ -7,14 +7,13 @@ class Pong(_Env):
     def __init__(self, render):
         super().__init__('Pong-v0', render)
         self.action_number = 6
-        self.state_number = (1, 84, 84)
+        self.state_number = (1, 210, 160)
     
     def reset(self):
         state = super().reset()
         state = np.array(state)
         state = state.mean(axis=2)
-        #state = np.resize(state, (84, 84))
-        #state = state.reshape((1, 84, 84))
+        state = state.reshape((1, 210, 160))
         return state
     
     def action(self, act):
@@ -24,6 +23,5 @@ class Pong(_Env):
         state, reward, done, _ = super().step(act)
         state = np.array(state)
         state = state.mean(axis=2)
-        state = np.resize(state, (84, 84))
-        state = state.reshape((1, 84, 84))
+        state = state.reshape((1, 210, 160))
         return state, reward, done, _
